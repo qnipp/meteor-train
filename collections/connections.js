7915fe436fa529349dba89cc310266d48dc2a341
Andi Pauschenwein <andreas@qnipp.com>
2015-10-11 16:15:21 +0200

    [REMOVE] renamed files.

--- collections/connections.js
+++
@@ -1,2 +0,0 @@
-//Connections = new Meteor.Collection("connections");
-UserPresences = new Meteor.Collection("userPresence");
--- server/publications/connections.js
+++
@@ -1,5 +0,0 @@
-Meteor.publish('userPresence', function() {
-  // Example of using a filter to publish only "online" users:
-  var filter = {state: "online"};
-  return UserPresences.find(filter);
-});
